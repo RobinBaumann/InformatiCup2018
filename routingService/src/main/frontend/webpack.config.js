d14bf880a29a816a8fad762b87a614b83948dabd
c-goetz <carlo.goetz@googlemail.com>
2018-01-13 02:49:18 +0100

    reduced bundle size by 600kb

--- routingService/src/main/frontend/webpack.config.js
+++ routingService/src/main/frontend/webpack.config.js
@@ -18,13 +18,6 @@ const config = {
         inline: true,
         port: 8080
     },
-    resolve: {
-        extensions: ['.ts', '.js', '.vue'],
-        alias: {
-            'vue$': 'vue/dist/vue.js',
-            'icons': path.resolve(__dirname, 'node_modules/vue-material-design-icons')
-        }
-    },
     module: {
         rules: [
             {
@@ -82,6 +75,13 @@ if (process.env.NODE_ENV === 'production') {
             new webpack.optimize.OccurrenceOrderPlugin()
         ]
     );
+    config.resolve = {
+        extensions: ['.ts', '.js', '.vue'],
+        alias: {
+            'vue$': 'vue/dist/vue.min.js',
+            'icons': path.resolve(__dirname, 'node_modules/vue-material-design-icons')
+        }
+    };
 } else {
     config.devtool = '#eval-source-map';
     config.module.rules = config.module.rules.concat(
@@ -99,7 +99,14 @@ if (process.env.NODE_ENV === 'production') {
             }),
             new BundleAnalyzerPlugin()
         ]
-    )
+    );
+    config.resolve = {
+        extensions: ['.ts', '.js', '.vue'],
+            alias: {
+            'vue$': 'vue/dist/vue.js',
+                'icons': path.resolve(__dirname, 'node_modules/vue-material-design-icons')
+        }
+    };
 }
 
 module.exports = config;
